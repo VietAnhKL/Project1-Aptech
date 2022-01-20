dd3c5ab74a65d5732d727c6219aa95308064ffaf
Viet Anh <vietanhtp99@gmail.com>
2022-01-20 21:45:47 +0700

    code project aptech(demo)

---
+++ admin/sign_in.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - youtube.com/codingnepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Animated Login Form | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="center">
+      <h1>Login</h1>
+      <form autocomplete="off" method="post" >
+        <div class="txt_field">
+          <input autocomplete="off" type="text" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input autocomplete="off" type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <input class="pass" type="checkbox"> Remember me!
+        <input type="submit" value="Login">
+        <div class="signup_link">
+          Not a member? <a href="sign_up.php">Sign up</a>
+        </div>
+      </form>
+    </div>
+
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,4 @@
+function scrollToTop() {
+    alert("hi");
+    window.scrollTo(0, 0);
+}
